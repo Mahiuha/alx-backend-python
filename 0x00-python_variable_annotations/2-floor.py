c02711c8d420883d3b5453ddf0e0959a7dc0b9ac
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-17 00:06:12 +0300

    returns floor of the float

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-""" 
-2. Basic annotations - floor
+""" 2. Basic annotations - floor
 """
 
 
 def floor(n: float) -> int:
-    """ 
-    Returns floor of number
+    """ Returns floor of number
     """
     return int(n) if n >= 0 else int(n) - 1
